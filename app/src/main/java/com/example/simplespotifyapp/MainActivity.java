12e4c4e6b648e99d9700f33770cb45797bd93240
jamesowen <james.o@disroot.org>
2021-05-02 09:50:27 +0100

    authorization update

--- app/src/main/java/com/example/simplespotifyapp/MainActivity.java
+++ app/src/main/java/com/example/simplespotifyapp/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         OkHttpClient client = new OkHttpClient();
 
-        String url = "https://accounts.spotify.com/authorize";
+        String url = "https://accounts.spotify.com/authorize?client_id=7406a90fa7c24810a377cccb6bf542fc&response_type=code&redirect_uri=http://localhost:8888/callback/";
 
         Request request = new Request.Builder()
                 .url(url)
